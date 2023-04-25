2f0750dd50735a211dc635860f674224c2f6588d
justin <justin@crazyshit>
2023-04-26 01:35:47 +0530

    sample first

---
+++ src/bank/Bank.java
@@ -0,0 +1,18 @@
+
+package bank;
+
+/**
+ *
+ * @author justin
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginPage s = new LoginPage();
+        s.setVisible(true);
+    }
+    
+}
